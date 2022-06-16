d7eba7940f7aef95d6d59ea87dd9721a86ed42e8
JayRalph360 <jayralph360@gmail.com>
2022-06-16 04:35:29 +0000

    Created a django blog app- Django model

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from msvcrt import kbhit
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(auto_now=True)
